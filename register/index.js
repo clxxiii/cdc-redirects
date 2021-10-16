673209f206bf6db568b0375edced27e04cdd5592
clxxiii <earthnoartisjusteh@gmail.com>
2021-10-16 00:36:26 -0400

    discord + much cleaner code

--- register/index.js
+++ register/index.js
@@ -1,21 +1,15 @@
-async function player() {
+async function redirect(url) {
   animateUp();
-  await sleep(500);
-  window.location.href = "https://docs.google.com/forms/d/e/1FAIpQLScMuOom3WgmTB8jn4LSwfhlTjhghC01gto85nw9ELTyfZDBXw/viewform?usp=sf_link"
+  sleep(500);
+  window.location.href = url;
 }
 
-async function staff() {
-  animateUp();
-  await sleep(500);
-  window.location.href = "https://docs.google.com/forms/d/e/1FAIpQLSeC2ZO_nPfC2AX1zC3G5EXwDC2GksOi56DUNw30jCIPSVvZnQ/viewform?usp=sf_link"
-}
-
-function onLoad() {
+async function onLoad() {
   document.getElementById('body').style.filter = "opacity(100%)"
   document.getElementById('body').style.transform = "translateY(0px)"
 }
 
-function animateUp() {
+async function animateUp() {
   document.getElementById('body').style.filter = "opacity(0%)"
   document.getElementById('body').style.transform = "translateY(-1000px)"
 }
--- src/index.js
+++ src/index.js
@@ -1,31 +1,7 @@
-async function forum() {
+async function redirect(url) {
   animateUp();
-  await sleep(500);
-  window.location.href = "https://osu.ppy.sh/community/forums"
-}
-
-async function sheet() {
-  animateUp();
-  await sleep(500);
-  window.location.href = "https://docs.google.com/forms/d/e/1FAIpQLScMuOom3WgmTB8jn4LSwfhlTjhghC01gto85nw9ELTyfZDBXw/viewform?usp=sf_link"
-}
-
-async function reg() {
-  animateUp();
-  await sleep(500);
-  window.location.href = "https://cdc.clxxiii.dev/register"
-}
-
-async function challonge() {
-  animateUp();
-  await sleep(500);
-  window.location.href = "https://challonge.com/cdc2021"
-}
-
-async function twitch() {
-  animateUp();
-  await sleep(500);
-  window.location.href = "https://twitch.tv/clxxiiis_tournaments"
+  sleep(500);
+  window.location.href = url;
 }
 
 async function onLoad() {
