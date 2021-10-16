a5de24790d738bf779c45b726bde1c2624c4709f
clxxiii <earthnoartisjusteh@gmail.com>
2021-10-15 23:48:02 -0400

    added load animations

---
+++ src/index.js
@@ -0,0 +1,24 @@
+function forum() {
+  window.location.href = "https://osu.ppy.sh/community/forums"
+}
+
+function sheet() {
+  window.location.href = "https://docs.google.com/forms/d/e/1FAIpQLScMuOom3WgmTB8jn4LSwfhlTjhghC01gto85nw9ELTyfZDBXw/viewform?usp=sf_link"
+}
+
+function reg() {
+  window.location.href = "https://cdc.clxxiii.dev/register"
+}
+
+function challonge() {
+  window.location.href = "https://challonge.com/cdc2021"
+}
+
+function twitch() {
+  window.location.href = "https://twitch.tv/clxxiiis_tournaments"
+}
+
+function onLoad() {
+  document.getElementById('body').style.filter = "opacity(100%)"
+  document.getElementById('body').style.transform = "translateY(0px)"
+}
