18952b2c411917915d48ed9c85e08b5eb38f4c9e
clxxiii <earthnoartisjusteh@gmail.com>
2021-10-15 23:20:26 -0400

    register site added

---
+++ register/index.js
@@ -0,0 +1,7 @@
+function player() {
+  window.location.href = "https://docs.google.com/forms/d/e/1FAIpQLScMuOom3WgmTB8jn4LSwfhlTjhghC01gto85nw9ELTyfZDBXw/viewform?usp=sf_link"
+}
+
+function staff() {
+  window.location.href = "https://docs.google.com/forms/d/e/1FAIpQLSeC2ZO_nPfC2AX1zC3G5EXwDC2GksOi56DUNw30jCIPSVvZnQ/viewform?usp=sf_link"
+}
