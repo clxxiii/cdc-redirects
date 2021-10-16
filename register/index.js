977a7dc11bdc7c9fa063b73b0578f7af3f2de28c
clxxiii <earthnoartisjusteh@gmail.com>
2021-10-15 23:57:05 -0400

    improved animations

--- register/index.js
+++ register/index.js
@@ -1,12 +1,25 @@
-function player() {
+async function player() {
+  animateUp();
+  await sleep(500);
   window.location.href = "https://docs.google.com/forms/d/e/1FAIpQLScMuOom3WgmTB8jn4LSwfhlTjhghC01gto85nw9ELTyfZDBXw/viewform?usp=sf_link"
 }
 
-function staff() {
+async function staff() {
+  animateUp();
+  await sleep(500);
   window.location.href = "https://docs.google.com/forms/d/e/1FAIpQLSeC2ZO_nPfC2AX1zC3G5EXwDC2GksOi56DUNw30jCIPSVvZnQ/viewform?usp=sf_link"
 }
 
 function onLoad() {
   document.getElementById('body').style.filter = "opacity(100%)"
   document.getElementById('body').style.transform = "translateY(0px)"
+}
+
+function animateUp() {
+  document.getElementById('body').style.filter = "opacity(0%)"
+  document.getElementById('body').style.transform = "translateY(-1000px)"
+}
+
+async function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms))
 }
--- src/index.js
+++ src/index.js
@@ -1,24 +1,43 @@
-function forum() {
+async function forum() {
+  animateUp();
+  await sleep(500);
   window.location.href = "https://osu.ppy.sh/community/forums"
 }
 
-function sheet() {
+async function sheet() {
+  animateUp();
+  await sleep(500);
   window.location.href = "https://docs.google.com/forms/d/e/1FAIpQLScMuOom3WgmTB8jn4LSwfhlTjhghC01gto85nw9ELTyfZDBXw/viewform?usp=sf_link"
 }
 
-function reg() {
+async function reg() {
+  animateUp();
+  await sleep(500);
   window.location.href = "https://cdc.clxxiii.dev/register"
 }
 
-function challonge() {
+async function challonge() {
+  animateUp();
+  await sleep(500);
   window.location.href = "https://challonge.com/cdc2021"
 }
 
-function twitch() {
+async function twitch() {
+  animateUp();
+  await sleep(500);
   window.location.href = "https://twitch.tv/clxxiiis_tournaments"
 }
 
-function onLoad() {
+async function onLoad() {
   document.getElementById('body').style.filter = "opacity(100%)"
   document.getElementById('body').style.transform = "translateY(0px)"
+}
+
+async function animateUp() {
+  document.getElementById('body').style.filter = "opacity(0%)"
+  document.getElementById('body').style.transform = "translateY(-1000px)"
+}
+
+async function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms))
 }
