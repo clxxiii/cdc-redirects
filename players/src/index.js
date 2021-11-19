2bbd7a9b235306ffc4e0b5be0817e137919e3c15
clxxiii <earthnoartisjusteh@gmail.com>
2021-11-19 18:13:52 -0500

    tier format -> player format

--- players/src/index.js
+++ players/src/index.js
@@ -7,9 +7,10 @@ async function onLoad() {
     let response = await fetch("https://script.google.com/macros/s/AKfycbwyauATAA4xKxlN-dR0LNPYaFdd_i1u60mJ_WuJt3Tr1bUT8E-CemuEgfcPX0hVpO56ag/exec")
     response = await response.json()
 
-    buildPlayerContainers(tier1, response.tier1);
-    buildPlayerContainers(tier2, response.tier2);
-    buildPlayerContainers(tier3, response.tier3);
-    buildPlayerContainers(tier4, response.tier4);
+    // buildPlayerContainers(tier1, response.tier1);
+    // buildPlayerContainers(tier2, response.tier2);
+    // buildPlayerContainers(tier3, response.tier3);
+    // buildPlayerContainers(tier4, response.tier4);
+    buildPlayerContainers(parse, response.parse);
 
 }
