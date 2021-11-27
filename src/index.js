8f4bca3dbed37c10090b33b0f50a7b1f5da92fab
clxxiii <earthnoartisjusteh@gmail.com>
2021-11-27 09:21:38 -0500

    combining javascripts into one file

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const urlParams = new URLSearchParams(queryString);
 
 async function redirect(url) {
   animateUp();
-  sleep(500);
+  await sleep(500);
   window.location.href = url;
 }
 
@@ -17,6 +17,8 @@ async function onLoad() {
 async function animateUp() {
   document.getElementById('elements').style.filter = "opacity(0%)"
   document.getElementById('elements').style.transform = "translateY(-1000px)"
+  await sleep(1000)
+  document.getElementById('elements').style.transform = "translateY(0)"
 }
 
 async function sleep(ms) {
@@ -24,18 +26,23 @@ async function sleep(ms) {
 }
 
 function sendAlerts() {
-  let alertBox = document.getElementById('alert')
-  let alert = urlParams.get('alert');
-  if (alert == "player-register") {
-    alertBox.innerHTML = "Your player registration was successful, Good Luck!"
+  try {
+    let alertBox = document.getElementById('alert')
+    let alert = urlParams.get('alert');
+    if (alert == "player-register") {
+      alertBox.innerHTML = "Your player registration was successful, Good Luck!"
+    }
+    else if (alert == "staff-register") {
+      alertBox.innerHTML = "You will be contacted on discord if your application is accepted. Thanks for applying!"
+    }
+    else if (alert == null) {
+      alertBox.style.filter = "opacity(0)"
+    }
+    else {
+      alertBox.innerHTML = alert
+    }
   }
-  else if (alert == "staff-register") {
-    alertBox.innerHTML = "You will be contacted on discord if your application is accepted. Thanks for applying!"
-  }
-  else if (alert == null) {
-    alertBox.style.filter = "opacity(0)"
-  }
-  else {
-    alertBox.innerHTML = alert
+  catch (e) {
+    console.log("Ran into error while trying to send an alert: " + e.message);
   }
 }
