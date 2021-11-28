dda7b2d621d792693af1304d67606d903203bc9d
clxxiii <earthnoartisjusteh@gmail.com>
2021-11-27 21:42:04 -0500

    added eliminated players section

--- players/src/index.js
+++ players/src/index.js
@@ -2,6 +2,7 @@ const tier1 = document.getElementById("tier1")
 const tier2 = document.getElementById("tier2")
 const tier3 = document.getElementById("tier3")
 const tier4 = document.getElementById("tier4")
+const eliminated = document.getElementById("eliminated")
 
 async function onLoad() {
     let response = await fetch("https://script.google.com/macros/s/AKfycbwyauATAA4xKxlN-dR0LNPYaFdd_i1u60mJ_WuJt3Tr1bUT8E-CemuEgfcPX0hVpO56ag/exec")
@@ -12,6 +13,7 @@ async function onLoad() {
     buildPlayerContainers(tier2, response.tier2);
     buildPlayerContainers(tier3, response.tier3);
     buildPlayerContainers(tier4, response.tier4);
+    buildPlayerContainers(eliminated, response.eliminated);
 
 }
 
